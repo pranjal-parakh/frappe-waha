9cf83722915e92c286cd529f2bdaf3617a0e6d83
pranjal-parakh <pranjalxparakh@gmail.com>
2025-07-29 19:02:56 +0530

    Update payment.py

--- wahawhatsapp/payment.py
+++ wahawhatsapp/payment.py
@@ -8,7 +8,7 @@ def payment_confirmation():
     # if data.get("status") == "1":
 
         session_doc = frappe.get_doc("user sessions", "916265064809@c.us")
-        session_doc.payment_status = "unpaid"
+        session_doc.payment_status = "paid"
         session_doc.save(ignore_permissions=True)
         frappe.db.commit()
 
